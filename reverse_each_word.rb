6e683410000132b51564509051fc16093db60e58
Desiree Marquez <marquezdecardona@gmail.com>
2020-07-16 16:48:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,14 @@
+
+
+
+
+
+
+
+
 def reverse_each_word(sentence)
   new_sentence= sentence.split
-  reverse_array = new_sentence.map do |word|
+  reverse_array = new_sentence.each do |word|
   word.reverse
 end
  reverse_array.join (" ")
