9a7399707da589b1cf4d0d4ce44f7a26a6b7bc9c
Desiree Marquez <marquezdecardona@gmail.com>
2020-07-16 16:38:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,11 @@
 def reverse_each_word(sentence)
   new_sentence= sentence.split
-  
-  end
-
-  end
-  
-  
-  
   reverse_array = new_sentence.map do |word|
   word.reverse
 end
+  end
+
+
+  
+  
+  
