bf4ec9461694c2e98e61933a46753135b8d588ad
Desiree Marquez <marquezdecardona@gmail.com>
2020-07-16 16:52:10 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,25 @@
+
+def reverse_each_word(sentence)
+  new_sentence= sentence.split
+  reverse_array = new_sentence.each do |word|
+  word.reverse
+end
+ reverse_array.join (" ")
+  end
+
+
+
+
+
+def reverse_each_word(sentence)
+  new_sentence= sentence.split
+  reverse_array = new_sentence.collect do |word|
+  word.reverse
+end
+ reverse_array.join (" ")
+  end
+
+
+  
+  
+  
